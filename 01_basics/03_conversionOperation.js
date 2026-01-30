d410828c1efeac7775dd2868c7f3a1c21b899cff
Shresth Jasiwal <138478466+Shresth-11@users.noreply.github.com>
2026-01-30 11:26:15 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 
 let isLoggedin = "hitesh"
 let booleanIsLoggedIn = Boolean(isLoggedin)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 
@@ -21,6 +21,40 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ************** Operations *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
 
