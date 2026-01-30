de4c647f75cf173bfa1fae0197ef9cdb6be23409
Shresth Jasiwal <138478466+Shresth-11@users.noreply.github.com>
2026-01-30 10:07:28 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,17 @@ const accountId = 14453
 let accountEmail = "shresth@gmail.com"
 var accountPass = "12345"
 accountCity = "Jaipur"
+
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountCity,accountPass, accountState]);
+
+/*
+Prefer not use var 
+because of issue in function scope and block scope
+*/
