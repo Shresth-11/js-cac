02014a23145578006924b2633bb9c9565ce16f5c
Shresth Jasiwal <138478466+Shresth-11@users.noreply.github.com>
2026-01-30 11:02:30 +0000

    conversion of datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser, since javascript engine resides in the browser, but we can use this feature (alert) with different method in node too.
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value and dataType too, null is both.
+// undefined => variable declared but value is not given.
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+console.table([typeof undefined, typeof null, typeof isLoggedIn, typeof state, typeof name]);
+
+
