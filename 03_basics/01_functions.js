aa04573f254e1b849b2a50318263b381d281ad10
Shresth Jasiwal <138478466+Shresth-11@users.noreply.github.com>
2026-02-01 05:53:04 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,15 +27,44 @@ const result = addTwoNumbers(3, 5)
 
 function loginUserMessage(username = "Shresth"){
     if(!username){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return
     }
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("Shresth"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
 
+function calculateCartPrice(val1, val2, ...num){
+    return num 
+}
 
- 
+// console.log(calculateCartPrice(100,200,300,400,3000));
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){   
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+ handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
