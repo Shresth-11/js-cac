0a5cbce3334d76c7cf47f2c13b1610e0b2bdfb1c
Shresth Jasiwal <138478466+Shresth-11@users.noreply.github.com>
2026-02-02 13:43:23 +0000

    for-in

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,3 +5,27 @@ const myObject = {
     swift: 'swift by apple'
 }
 
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+// }
+
+
+const programming = ["js", "ruby", "py", "java", "cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);    
+}
+
+
+
+const map = new Map()   
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+
+for (const key in map) {  // maps are not iterable use for of loop
+    console.log(key);
+}
+
